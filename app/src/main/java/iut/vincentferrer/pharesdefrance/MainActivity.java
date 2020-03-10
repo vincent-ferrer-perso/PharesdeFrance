d1b3f58e05633633192714d19122d08ddb9953c8
vincent ferrer <vincent.ferrer@etu.univ-amu.fr>
2020-03-10 09:09:39 +0100

    login verif pwd

--- app/src/main/java/iut/vincentferrer/pharesdefrance/MainActivity.java
+++ app/src/main/java/iut/vincentferrer/pharesdefrance/MainActivity.java
@@ -3,12 +3,30 @@ package iut.vincentferrer.pharesdefrance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    final static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        ((Button)findViewById(R.id.buttonLogin)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pwd = ((EditText)findViewById(R.id.password)).getText().toString();
+                if (pwd.equals("xxx")) {
+                    Log.d(TAG, "Welcome");
+                } else {
+                    Log.d(TAG, "invalid passwd");
+                }
+            }
+        });
     }
 }
